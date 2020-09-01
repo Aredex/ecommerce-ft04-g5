5f9a27b3e10660a696c4402070d894fcaafd8aa6
Diego Rodriguez <dierodz91@gmail.com>
2020-09-01 08:50:19 -0300

    prepare

---
+++ client/src/Routes/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Routes = () => {
+  return <div></div>;
+};
+
+export default Routes;
---
+++ client/src/components/Layout/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Layout = (props) => {
+  return <div></div>;
+};
+
+Layout.propTypes = {};
+
+const Header = (props) => {
+  return <div></div>;
+};
+
+Header.propTypes = {};
+
+const Content = (props) => {
+  return <div></div>;
+};
+
+Content.propTypes = {};
+
+const Footer = (props) => {
+  return <div></div>;
+};
+
+Footer.propTypes = {};
+
+Layout.Header = Header;
+Layout.Content = Content;
+Layout.Footer = Footer;
+
+export default Layout;
