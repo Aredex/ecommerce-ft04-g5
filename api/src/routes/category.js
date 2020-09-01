3bd7c10d6ee1093d3eb1ad7c8e359ebf59ce57ec
Aredex <pacuestar@gmail.com>
2020-09-01 10:48:33 -0500

    funciona eliminary buscar uno

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -6,7 +6,10 @@ router
     .get((req, res) => {
         Category.findAll({})
             .then((categories) => {
-                res.json(categories);
+                res.json({
+                    message: "Success",
+                    categories,
+                });
             })
             .catch((err) => {
                 res.json({
@@ -20,10 +23,79 @@ router
 
         Category.create({ name, description })
             .then((category) => {
-                res.status(201).json(category);
+                res.status(201).json({
+                    message: "Success",
+                    category,
+                });
             })
             .catch((err) => {
                 res.status(400).json({
+                    message: "error",
+                    error: err.message,
+                });
+            });
+    });
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        const { id } = req.params;
+
+        Category.findOne({
+            where: { id },
+        })
+            .then((category) => {
+                res.json({
+                    message: "Success",
+                    category,
+                });
+            })
+            .catch((err) => {
+                res.json({
+                    message: "error",
+                    error: err.message,
+                });
+            });
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { name, description } = req.body;
+        Category.findOne({
+            where: { id },
+        })
+            .then((category) => {
+                category.name = name;
+                category.description = description;
+
+                return category.save();
+            })
+            .then((category) => {
+                res.json({
+                    message: "Success",
+                    category,
+                });
+            })
+            .catch((err) => {
+                res.json({
+                    message: "error",
+                    error: err.message,
+                });
+            });
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        Category.findOne({
+            where: { id },
+        })
+            .then((category) => {
+                category.destroy();
+
+                res.json({
+                    message: "Success",
+                });
+            })
+            .catch((err) => {
+                res.json({
                     message: "error",
                     error: err.message,
                 });
