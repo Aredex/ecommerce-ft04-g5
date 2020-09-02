3a7e8310d6d905041c99546f1eaf354c2c6aaf1d
Diego Rodriguez <dierodz91@gmail.com>
2020-09-02 13:20:45 -0300

    fix merge

--- api/src/controllers/products.js
+++ api/src/controllers/products.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Product, Category, Op } = require("../db");
 
 // ==============================================
@@ -6,97 +5,21 @@ const { Product, Category, Op } = require("../db");
 // ==============================================
 
 const getAll = () => {
-    return new Promise((resolve, reject) => {
-        Product.findAll({})
-            .then((products) => resolve(products))
-            .catch((err) => reject({ error: err }));
-    });
+  return new Promise((resolve, reject) => {
+    Product.findAll({})
+      .then((products) => resolve(products))
+      .catch((err) => reject({ error: err }));
+  });
 };
 
 const createOne = (name, description, price) => {
-    return new Promise((resolve, reject) => {
-        if (!name || !description || !price) {
-            return reject({ error: "Uno o mas parametros faltantes" });
-        }
-        Product.create({ name, description, price })
-            .then((products) => resolve(products))
-            .catch((err) => reject({ error: err.message }));
-    });
-};
-
-const getOne = (id) => {
-    return new Promise((resolve, reject) => {
-        Product.findOne({ where: { id }, include: [Category] })
-            .then((product) => {
-                if (!product) {
-                    return reject({ error: "No existe en la BD" });
-                }
-
-                resolve(product);
-            })
-            .catch((err) => reject({ error: err.message }));
-    });
-};
-
-const editOne = (id, name, description, price) => {
-    return new Promise((resolve, reject) => {
-        getOne(id)
-            .then((product) => {
-                product.name = name;
-                product.description = description;
-                product.price = price;
-
-                return product.save();
-            })
-            .then((product) => resolve(product))
-            .catch((err) => reject(err));
-    });
-};
-
-const getByQuery = (query) => {
-    return new Promise((resolve, reject) => {
-        if (!query) {
-            return reject({ error: "Se necesita parámetro de búsqueda" });
-        }
-
-        Product.findAll({
-            where: {
-                [Op.or]: [
-                    { name: { [Op.like]: `%${query}%` } },
-                    { description: { [Op.like]: `%${query}%` } },
-                ],
-            },
-        })
-            .then((products) => resolve(products))
-            .catch(() =>
-                reject({
-                    error: "No hay productos que conicidan con la búsqueda",
-                })
-            );
-    });
-};
-
-module.exports = {
-    getAll,
-    createOne,
-    getOne,
-    editOne,
-    getByQuery,
-=======
-const { Product, Category } = require("../db");
-const { addCategory } = require("./products_categories");
-
-const getAll = () => {
   return new Promise((resolve, reject) => {
-    Product.findAll({})
-      .then((products) => {
-        resolve(products);
-      })
-      .catch((err) => {
-        reject({
-          error: err,
-        });
-      });
+    if (!name || !description || !price) {
+      return reject({ error: "Uno o mas parametros faltantes" });
+    }
+    Product.create({ name, description, price })
+      .then((products) => resolve(products))
+      .catch((err) => reject({ error: err.message }));
   });
 };
 
@@ -114,27 +37,13 @@ const getOne = (id) => {
   });
 };
 
-const createOne = (name, description, price) => {
-  return new Promise((resolve, reject) => {
-    if (!name || !description || !price) {
-      return reject({
-        error: "Uno o mas parametros faltantes",
-      });
-    }
-    Product.create({ name, description, price })
-      .then((products) => resolve(products))
-      .catch((err) => reject({ error: err.message }));
-  });
-};
-
-const editOne = (id, name, description, price, stock, categoryName) => {
+const editOne = (id, name, description, price) => {
   return new Promise((resolve, reject) => {
     getOne(id)
       .then((product) => {
         product.name = name;
         product.description = description;
         product.price = price;
-        product.stock = stock;
 
         return product.save();
       })
@@ -143,26 +52,33 @@ const editOne = (id, name, description, price, stock, categoryName) => {
   });
 };
 
-const deleteOne = (id) => {
+const getByQuery = (query) => {
   return new Promise((resolve, reject) => {
-    console.log(id);
-    Product.findByPk(id)
-      .then((product) => {
-        if (!product) {
-          return reject({ error: "No existe en la BD" });
-        }
-        product.destroy();
-        resolve(product);
-      })
-      .catch((err) => reject(err));
+    if (!query) {
+      return reject({ error: "Se necesita parámetro de búsqueda" });
+    }
+
+    Product.findAll({
+      where: {
+        [Op.or]: [
+          { name: { [Op.like]: `%${query}%` } },
+          { description: { [Op.like]: `%${query}%` } },
+        ],
+      },
+    })
+      .then((products) => resolve(products))
+      .catch(() =>
+        reject({
+          error: "No hay productos que conicidan con la búsqueda",
+        })
+      );
   });
 };
 
 module.exports = {
   getAll,
-  getOne,
   createOne,
+  getOne,
   editOne,
-  deleteOne,
->>>>>>> dev
+  getByQuery,
 };
--- api/src/controllers/products_categories.js
+++ api/src/controllers/products_categories.js
@@ -1,6 +1,5 @@
 const { getOne: getProduct } = require("./products");
 const { getOne: getCategory } = require("./categories");
-<<<<<<< HEAD
 // ==============================================
 //      ESTOS METODOS RETORNAN PROMESAS
 // ==============================================
@@ -8,39 +7,23 @@ const { getOne: getCategory } = require("./categories");
 // Dado el Id de un producto y el Id de una categoría
 // Agrega dicha categoría a dicho producto
 const addCategory = async (IdProduct, IdCategory) => {
-    const Product = await getProduct(IdProduct);
-    const Category = await getCategory(IdCategory);
-
-    return new Promise((resolve, reject) => {
-        Product.addCategory(Category) // Agregamos la categoría
-            .then((product_category) => resolve(product_category))
-            .catch((err) => reject(err));
-    });
-};
-
-// TODO Falta implementar en las rutas y comprobar funcionamiento
-const removeCategory = async (IdProduct, IdCategory) => {
-    const Product = await getProduct(IdProduct);
-    const Category = await getCategory(IdCategory);
-
-    return new Promise((resolve, reject) => {
-        Product.removeCategory(Category)
-            .then((product_category) => resolve(product_category))
-            .catch((err) => reject(err));
-    });
-};
-
-module.exports = {
-    addCategory,
-    removeCategory,
-=======
-
-const addCategory = async (IdProduct, IdCategory) => {
   const Product = await getProduct(IdProduct);
   const Category = await getCategory(IdCategory);
 
   return new Promise((resolve, reject) => {
-    Product.addCategory(Category)
+    Product.addCategory(Category) // Agregamos la categoría
+      .then((product_category) => resolve(product_category))
+      .catch((err) => reject(err));
+  });
+};
+
+// TODO Falta implementar en las rutas y comprobar funcionamiento
+const removeCategory = async (IdProduct, IdCategory) => {
+  const Product = await getProduct(IdProduct);
+  const Category = await getCategory(IdCategory);
+
+  return new Promise((resolve, reject) => {
+    Product.removeCategory(Category)
       .then((product_category) => resolve(product_category))
       .catch((err) => reject(err));
   });
@@ -48,5 +31,5 @@ const addCategory = async (IdProduct, IdCategory) => {
 
 module.exports = {
   addCategory,
->>>>>>> dev
+  removeCategory,
 };
