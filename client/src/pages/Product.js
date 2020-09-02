d6d97c8aba1684bdacb1e22e552936eaf549494a
TheJB4 <juanmer382@gmail.com>
2020-09-02 10:28:30 -0300

    Cambios realizados

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,54 +1,55 @@
 import React from "react";
 import style from "./Product.module.scss";
 
-class Product extends React.Component {
-  state = {
-    count: 0,
+const Product = () => {
+  const [count, setCount] = React.useState(1);
+  const [stock, setStock] = React.useState(20);
+
+  const handleClick_suma = () => {
+    setCount(count + 1);
   };
 
-  handleClick_suma = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  handleClick_resta = () => {
-    if (this.state.count <= 1) {
+  const handleClick_resta = () => {
+    if (count <= 1) {
       return 1;
     }
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
 
-  render() {
-    const { count } = this.state;
-    return (
-      <div>
-        <img
-          src="https://laslandas.com/wp-content/uploads/2019/11/abbaye-cluny1.jpg"
-          alt="FotoPlanta"
-          width="200"
-          height="200"
-        ></img>
-        <h1>Abbaye de Cluny</h1>
-        <p>$450</p>
-        <p>Descripcion</p>
-        <p>Tipo:Híbrida de té</p>
-        <button>
-          <i className={["fas", "fa-shopping-cart", style.icon].join(" ")}></i>
-          Añadir al Carro
-        </button>
-        <input placeholder={count}></input>
-        <button onClick={this.handleClick_suma}>
-          <i className={["fas", "fa-angle-up", style.icon].join(" ")}></i>
-        </button>
-        <button onClick={this.handleClick_resta}>
-          <i className={["fas", "fa-angle-down", style.icon].join(" ")}></i>
-        </button>
-      </div>
-    );
-  }
-}
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <img
+        src="https://laslandas.com/wp-content/uploads/2019/11/abbaye-cluny1.jpg"
+        alt="FotoPlanta"
+        width="200"
+        height="200"
+      ></img>
+      <h1>Abbaye de Cluny</h1>
+      <p>$450</p>
+      <p>Descripcion</p>
+      <p>Tipo:Híbrida de té</p>
+
+      {stock > 0 ? (
+        <>
+          <button>
+            <i
+              className={["fas", "fa-shopping-cart", style.icon].join(" ")}
+            ></i>
+            Añadir al Carro
+          </button>
+          <input value={count} readOnly></input>
+          <button onClick={handleClick_suma} disabled={count === stock}>
+            <i className={["fas", "fa-angle-up", style.icon].join(" ")}></i>
+          </button>
+          <button onClick={handleClick_resta} disabled={count === 1}>
+            <i className={["fas", "fa-angle-down", style.icon].join(" ")}></i>
+          </button>{" "}
+        </>
+      ) : (
+        <h1>No contamos con stock</h1>
+      )}
+    </div>
+  );
+};
 
 export default Product;
