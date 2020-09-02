a81992992b21277c6559e6967cd88eeff27118a2
Aredex <pacuestar@gmail.com>
2020-09-02 05:48:15 -0500

    implementando controlador en product route

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,54 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
+const router = require("express").Router();
+const { addCategory } = require("../controllers/products_categories");
+const {
+    getAll,
+    createOne,
+    editOne,
+    getOne,
+} = require("../controllers/products");
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
-});
+router
+    .route("/")
+    .get((req, res, next) => {
+        getAll()
+            .then((products) => {
+                res.send(products);
+            })
+            .catch(next);
+    })
+    .post((req, res) => {
+        const { name, description, price } = req.body;
 
-module.exports = server;
+        createOne(name, description, price)
+            .then((products) => res.status(201).json(products))
+            .catch((err) => res.status(400).json(err));
+    });
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        const { id } = req.params;
+        getOne(id)
+            .then((product) => res.json(product))
+            .catch((error) => res.status(400).json(error));
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { name, description, price, IdCategory } = req.body;
+
+        // Dado el caso que existe el IdCategory, significa que están agregando
+        // ese categoría, al producto.
+        if (IdCategory) {
+            return addCategory(id, IdCategory) // Llamamos al método del controlador
+                .then((product_category) =>
+                    res.json(product_category).status(201)
+                )
+                .catch((err) => res.json(err));
+        }
+
+        editOne(id, name, description, price)
+            .then((product) => res.json(product).status(201))
+            .catch((err) => res.json(err));
+    });
+
+module.exports = router;
