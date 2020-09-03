f8c5f90de22ba973089d6020d15418b5c31c2940
Diego Rodriguez <dierodz91@gmail.com>
2020-09-03 18:42:57 -0300

    Update index.js

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -5,11 +5,13 @@ import Product from "pages/Product";
 import SignIn from "pages/SignIn";
 import Admin from "pages/Admin";
 import Home from "pages/Home";
+import Products from "pages/Products";
 
 const Routes = () => {
   return (
     <>
       <Route exact path="/" component={Home}></Route>
+      <Route exact path="/products" component={Products} />
       <Route exact path="/product/:id" component={Product}></Route>
       <Route exact path="/sign-in" component={SignIn}></Route>
       <Route path="/admin" component={Admin}></Route>
