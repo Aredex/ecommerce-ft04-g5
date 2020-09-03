a50c72241ab355634e056bd60e5f20f1fcdc84a1
Ernesto Gonzalez Llano <ernestogonzalezllano@gmail.com>
2020-09-03 11:41:12 -0300

    ImplementacionSearchBar, se agregaron acciones Redux

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -3,10 +3,12 @@ import { Route } from "react-router-dom";
 import Home from "pages/Home";
 import Product from "pages/Product";
 import SignIn from "pages/SignIn";
+import SearchBar from "components/SearchBar/SearchBar"
 
 const Routes = () => {
   return (
     <div>
+      <Route path="/" component={SearchBar}></Route>
       <Route exact path="/" component={Home}></Route>
       <Route exact path="/product/:id" component={Product}></Route>
       <Route exact path="/sign-in" component={SignIn}></Route>
