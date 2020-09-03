d2fe4330a17720131c2274fc40971dfde938870f
TheJB4 <juanmer382@gmail.com>
2020-09-03 10:31:49 -0300

    S8-Form1

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import Product from '../pages/Product';
-
+import { Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Product from "../pages/Product";
+import CrudComponent from "../pages/Crud";
 
 const Routes = () => {
-  return <div>
-    <Route exact path='/' component={Home} ></Route>
-    <Route exact path='/product/:id' component={Product} ></Route>
-  </div>;
+  return (
+    <div>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/product/:id" component={Product}></Route>
+      <Route exact path="/CRUD/" component={CrudComponent}></Route>
+    </div>
+  );
 };
 
 export default Routes;
---
+++ client/src/pages/Crud.js
@@ -0,0 +1,36 @@
+import React from "react";
+import style from "./Product.module.scss";
+
+const Product = () => {
+  return (
+    <div className={"style.commentAddWrapper"}>
+      {/* <div className={"style.commentAddBtn"} onClick={""}>
+        <span>+</span>
+      </div> */}
+      <div className={"style.modalClasses"}>
+        <h2>Añadi tu nueva planta!</h2>
+        <form onSubmit={"this.addComment.bind(this)"}>
+          <p>Titulo</p>
+          <input></input>
+          <p>Precio</p>
+          <input></input>
+          <p>Stock</p>
+          <input></input>
+          <br />
+          <button
+            type="button"
+            className={"style.btnDefault"}
+            onClick={"this.toggleModal.bind(this)"}
+          >
+            Cancelar
+          </button>
+          <button type="submit" className={"style.btnSuccess"}>
+            Añadir
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
