dca4d03e5206107126e6f812ed8a5544a4f64b80
Aredex <pacuestar@gmail.com>
2020-09-03 17:57:12 -0500

    agregando los valores del status de las órdenes

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,17 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
-  // defino el modelo
-  return sequelize.define("order", {
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    return sequelize.define("order", {
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.ENUM,
+            values: [
+                "IN CREATION",
+                "CONFIRMED",
+                "REJECTED",
+                "PREPARING",
+                "SENT",
+                "DELIVERED",
+                "FINALIZED",
+            ],
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
 };
