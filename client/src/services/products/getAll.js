c119d6715a791a1709e211c6fd5b4f056f7477f9
Diego Rodriguez <dierodz91@gmail.com>
2020-09-03 09:21:59 -0300

    fix getAll

--- client/src/services/products/getAll.js
+++ client/src/services/products/getAll.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export default async function getById(id) {
-    return axios
-        .get(`http://localhost:3001/products`)
-        .then((response) => {
-            return response.data;
-        })
-        .catch(() => {
-            return undefined;
-        });
+export default async function getAll() {
+  return axios
+    .get(`http://localhost:3001/products`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch(() => {
+      return undefined;
+    });
 }
