ba7cf23e2d2635d3befe703ace0a05eab8c6c30e
Diego Rodriguez <dierodz91@gmail.com>
2020-09-03 16:45:47 -0300

    style

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -3,19 +3,21 @@ import { connect } from "react-redux";
 import { searchProduct } from "../../store/Actions/Actions.js";
 import style from "./index.module.scss";
 
-
 const SearchBar = (props) => {
   const inputa = React.useRef(null);
-
 
   return (
     <div className={style.inputNumber}>
       <input className="Inp" type="text" ref={inputa} placeholder="Búscalo!" />
-      <button className={style.submit} onClick={() => props.searchProduct(inputa.current.value)}> BUSCAR </button>
+      <button
+        className={style.submit}
+        onClick={() => props.searchProduct(inputa.current.value)}
+      >
+        <i className="fas fa-search"></i>
+      </button>
     </div>
-  )
-
-}
+  );
+};
 
 function mapStateToProps(state) {
   return {};
@@ -23,11 +25,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    searchProduct: filter => dispatch(searchProduct(filter))
+    searchProduct: (filter) => dispatch(searchProduct(filter)),
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
