08a2ae079d993edf36726b93428c3069652c5ab1
Sol Battaglia <solbattaglia@gmail.com>
2020-09-04 10:59:10 -0300

    Coreccion de status code en orders y agregado de la ruta /orders

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
+
 const { getAll } = require("../controllers/orders");
 
 router.route("/").get((req, res, next) => {
   return getAll()
     .then((products) => {
-      res.send(products);
+      res.send(products).status(200);
     })
     .catch(next);
 });
+
+module.exports = router;
