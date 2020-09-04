97efb6f861ce70ede12622c588d4c8ffd6190be4
Aredex <pacuestar@gmail.com>
2020-09-04 06:04:02 -0500

    finiquitando

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("user", {
+    return sequelize.define("users", {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
