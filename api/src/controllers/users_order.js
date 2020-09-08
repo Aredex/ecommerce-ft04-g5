b1cc45c23dd1da1d41c3e0187e8410b2735b7b81
Sol Battaglia <solbattaglia@gmail.com>
2020-09-08 09:05:51 -0300

    Some Changes not working yet

--- api/src/controllers/users_order.js
+++ api/src/controllers/users_order.js
@@ -1,35 +1,42 @@
-const {Order} = require('../db');
+const { Order_user } = require("../db");
 
-const returnOrder = (id) => {
-    return new Promise( (resolve, reject) => {
-            Order.findAll({
-            where: {
-                userId: id
-            }
-        })        
-        .then((orders)=> {
-            if(!id){
-                return reject({
-                    error: {
-                        errors: [
-                        {    message:
-                             "Id doesn't exist",
-                             type: "not found"
-                            },
-                        ],
-                    },
+const getOrderByUser = (userId) => {
+  return new Promise((resolve, reject) => {
+    Order_user.findAll({ where: { userId } })
+      .then((user_order) => resolve(user_order))
+      .catch((err) => reject({ error: err }));
+  });
+};
+// const returnOrder = (id) => {
+//     return new Promise( (resolve, reject) => {
+//             Order.findAll({
+//             where: {
+//                 userId: id
+//             }
+//         })
+//         .then((orders)=> {
+//             if(!id){
+//                 return reject({
+//                     error: {
+//                         errors: [
+//                         {    message:
+//                              "Id doesn't exist",
+//                              type: "not found"
+//                             },
+//                         ],
+//                     },
 
-                });
-            }
-            
-            resolve(orders);
-            })
-        .catch((err)=>reject ({error : err}))
-    
+//                 });
+//             }
 
-})
-}
+//             resolve(orders);
+//             })
+//         .catch((err)=>reject ({error : err}))
+
+// })
+// }
 
 module.exports = {
-  returnOrder
-}
+  //   returnOrder,
+  getOrderByUser,
+};
