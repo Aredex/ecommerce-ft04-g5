44f328e1d23981146b119c2999149db707a4a1e4
Aredex <pacuestar@gmail.com>
2020-09-08 12:09:32 -0500

    resolved

--- api/src/models/Review.js
+++ api/src/models/Review.js
@@ -1,38 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('review', {
-    stars: {
-      type: DataTypes.ENUM('1', '2', '3', '4', '5'),,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: false,
-      },
-      set(value) {
-        this.setDataValue("title", value.trim().toLowerCase());
-      }
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      validate: {
-        notEmpty: false,
-      },
-      set(value) {
-        this.setDataValue("description", value.trim());
-      },
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true,
-    },
-    productId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true,
-    }
-  })
-}
+    return sequelize.define("review", {
+        stars: {
+            type: DataTypes.ENUM("1", "2", "3", "4", "5"),
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: false,
+            },
+            set(value) {
+                this.setDataValue("title", value.trim().toLowerCase());
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: false,
+            },
+            set(value) {
+                this.setDataValue("description", value.trim());
+            },
+        },
+    });
+};
