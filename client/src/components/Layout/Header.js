85f425e02de161d008145779aa7d5af4c4e18573
Diego Rodriguez <dierodz91@gmail.com>
2020-09-09 08:38:05 -0300

    resolve create component navbar

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "components/SearchBar";
 import style from "./index.module.scss";
 import logo from "logo.svg";
+import { NavLink, useLocation, useHistory } from "react-router-dom";
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+  const location = useLocation();
+  const history = useHistory();
+  useEffect(() => {
+    setShowNav(false);
+  }, [location]);
   return (
     <header className={style.header}>
-      <button className={style.buttonMenu}>
-        <i className="fas fa-bars"></i>
-      </button>
-      <div className={style.headerBrand}>
+      <div className={style.nav}>
+        <button
+          className={style.buttonMenu}
+          onClick={() => setShowNav(!showNav)}
+        >
+          <i className="fas fa-bars"></i>
+        </button>
+        <nav className={showNav ? style.showNav : ""}>
+          <section>
+            <NavLink
+              exact
+              className={style.navLink}
+              activeClassName={style.activeNav}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={style.navLink}
+              activeClassName={style.activeNav}
+              to="/products"
+            >
+              Productos
+            </NavLink>
+            <NavLink
+              className={style.navLink}
+              activeClassName={style.activeNav}
+              to="/about"
+            >
+              Sobre nosotros
+            </NavLink>
+            <NavLink
+              className={style.navLink}
+              activeClassName={style.activeNav}
+              to="/admin"
+            >
+              Administración
+            </NavLink>
+          </section>
+          <section></section>
+        </nav>
+      </div>
+      <div className={style.headerBrand} onClick={() => history.push("/")}>
         <img className={style.logo} src={logo} alt="Logo" />
         <span className={style.brand}>GardenRy</span>
       </div>
