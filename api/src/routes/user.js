957c0b5a383ae61b5c4b6977fb42c8de3ca361ab
Aredex <pacuestar@gmail.com>
2020-09-09 18:46:54 -0500

    tab

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,69 +1,69 @@
 const router = require("express").Router();
 const {
-  createOne,
-  getAll,
-  getOne,
-  editOne,
-  deleteOne,
+    createOne,
+    getAll,
+    getOne,
+    editOne,
+    deleteOne,
 } = require("../controllers/users");
-const { getOrderByUser, setUsertoOrder } = require("../controllers/users_order");
+const {
+    getOrderByUser,
+    setUsertoOrder,
+} = require("../controllers/users_order");
 router
-  .route("/")
-  .post((req, res) => {
-    const { name, email, password, role } = req.body;
+    .route("/")
+    .post((req, res) => {
+        const { name, email, password, role } = req.body;
 
-    createOne(name, email, password, role)
-      .then((user) => res.status(201).json(user))
-      .catch((err) => res.status(400).json(err));
-  })
-  .get((req, res) => {
-    getAll()
-      .then((users) => res.json(users).status(200))
-      .catch((err) => res.status(404).json(err));
-  });
+        createOne(name, email, password, role)
+            .then((user) => res.status(201).json(user))
+            .catch((err) => res.status(400).json(err));
+    })
+    .get((req, res) => {
+        getAll()
+            .then((users) => res.json(users).status(200))
+            .catch((err) => res.status(404).json(err));
+    });
 
 router
-  .route("/:id")
-  .get((req, res) => {
-    const { id } = req.params;
+    .route("/:id")
+    .get((req, res) => {
+        const { id } = req.params;
 
-    getOne(id)
-      .then((user) => res.json(user).status(200))
-      .catch((err) => res.status(404).json(err));
-  })
-  .put((req, res) => {
-    const { id } = req.params;
-    const { name, email, password, role } = req.body;
+        getOne(id)
+            .then((user) => res.json(user).status(200))
+            .catch((err) => res.status(404).json(err));
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { name, email, password, role } = req.body;
 
-    editOne(id, name, email, password, role)
-      .then((user) => res.json(user).status(201))
-      .catch((err) => res.status(400).json({ err }));
-  })
-  .delete((req, res) => {
-    const { id } = req.params;
-    deleteOne(id)
-      .then((user) => res.json(user).status(200))
-      .catch((err) => res.status(400).json(err));
-  });
+        editOne(id, name, email, password, role)
+            .then((user) => res.json(user).status(201))
+            .catch((err) => res.status(400).json({ err }));
+    })
+    .delete((req, res) => {
+        const { id } = req.params;
+        deleteOne(id)
+            .then((user) => res.json(user).status(200))
+            .catch((err) => res.status(400).json(err));
+    });
 
 router.route("/:id/orders").get((req, res) => {
-  const { id } = req.params;
-  // returnOrder(id)
-  getOrderByUser(id)
-    .then((orders) => res.json(orders).status(200))
-    .catch((err) => res.json(err));
-});
+    const { id } = req.params;
 
+    getOrderByUser(id)
+        .then((orders) => res.json(orders).status(200))
+        .catch((err) => res.json(err));
+});
 
 //-------------------------------------------------------\\
 //solo para probar la petición de ordenes
 
 router.route("/:id/orders/:idOrder").put((req, res) => {
     const { id, idOrder } = req.params;
- setUsertoOrder(id, idOrder)
-    .then((orders) => res.json(orders).status(200))
-    .catch((err) => res.json(err));
-
-
+    setUsertoOrder(id, idOrder)
+        .then((orders) => res.json(orders).status(200))
+        .catch((err) => res.json(err));
 });
 module.exports = router;
