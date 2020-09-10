e33f68e71f8451ea9de814894d7a610dd7fbf9af
Sol Battaglia <solbattaglia@gmail.com>
2020-09-10 18:20:49 -0300

    creada ruta de ordenes por id de usuario

--- client/src/services/orders/getOrderByUserId.js
+++ client/src/services/orders/getOrderByUserId.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export default async function getAllOrders() {
+export default async function getOrderByUserId(id) {
   return axios
-    .get(`http://localhost:3001/orders`)
+    .get(`http://localhost:3001/users/${id}/orders`)
     .then((response) => {
       return response.data;
     })
@@ -10,5 +10,3 @@ export default async function getAllOrders() {
       return undefined;
     });
 }
-
-// TODO - importante para usar - OBTIENE TODAS LAS ORDENES
