69c7b672cff96a883081a5687f9a8ce0680bc631
Aredex <pacuestar@gmail.com>
2020-09-10 13:03:01 -0500

    solved

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,54 +1,53 @@
 const router = require("express").Router();
-const {
-  createOne,
-  getAll,
-  getOne,
-  editOne,
-  changeRole,
-} = require("../controllers/users");
+const { createOne, getAll, getOne, editOne } = require("../controllers/users");
 
 router
-  .route("/")
-  .post((req, res) => {
-    const { name, email, password, role } = req.body;
+    .route("/")
+    .post((req, res) => {
+        const { name, email, password, role } = req.body;
 
-    createOne(name, email, password, role)
-      .then((user) => res.json(user).status(201))
-      .catch((err) => res.status(400).json(err));
-  })
-  .get((req, res) => {
-    getAll()
-      .then((users) => res.json(users).status(200))
-      .catch((err) => res.status(404).json(err));
-  });
-//   .patch((req, res) => {
-//     changeRole()
-//       .then((user_role) => res.json(user_role))
-//       .catch((err) => res.status(400).json({ err }));
-//   });
+        createOne(name, email, password, role)
+            .then((user) => res.json(user).status(201))
+            .catch((err) => res.status(400).json(err));
+    })
+    .get((req, res) => {
+        getAll()
+            .then((users) => res.json(users).status(200))
+            .catch((err) => res.status(404).json(err));
+    });
 
 router
-  .route("/:id")
-  .get((req, res) => {
+    .route("/:id")
+    .get((req, res) => {
+        const { id } = req.params;
+
+        getOne(id)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(404).json(err));
+    })
+    .put((req, res) => {
+        const { id } = req.params;
+        const { name, email, password, role } = req.body;
+
+        editOne({ id, name, email, password, role })
+            .then((user) => res.json(user))
+            .catch((err) => res.status(400).json({ err }));
+    });
+
+router.route("/:id/toadmin").put((req, res) => {
     const { id } = req.params;
 
-    getOne(id)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(404).json(err));
-  })
-  .put((req, res) => {
-    const { id } = req.params;
-    const { name, email, password, role } = req.body;
+    editOne({ id, role: "ADMIN" })
+        .then((user) => res.json(user))
+        .catch((err) => res.status(400).json({ err }));
+});
 
-    editOne(id, name, email, password, role)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(400).json({ err }));
-  })
-  .patch((req, res) => {
+router.route("/:id/toguest").put((req, res) => {
     const { id } = req.params;
-    changeRole(id)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(404).json(err));
-  });
+
+    editOne({ id, role: "GUEST" })
+        .then((user) => res.json(user))
+        .catch((err) => res.status(400).json({ err }));
+});
 
 module.exports = router;
