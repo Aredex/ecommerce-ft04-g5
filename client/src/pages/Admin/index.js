0f1f6cdb33988f2388a4862bc51ccd8db023f2e0
Diego Rodriguez <dierodz91@gmail.com>
2020-09-10 21:20:36 -0300

    resolve users table

--- client/src/pages/Admin/index.js
+++ client/src/pages/Admin/index.js
@@ -3,14 +3,16 @@ import { Route } from "react-router";
 import style from "./index.module.scss";
 import Products from "./Products";
 import Categories from "./Categories";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Users from "./Users";
 
 const Admin = () => {
   return (
     <section className={style.page}>
       <aside>
-        <Link to="/admin/products">Productos</Link>
-        <Link to="/admin/categories">Categorías</Link>
+        <NavLink to="/admin/products">Productos</NavLink>
+        <NavLink to="/admin/categories">Categorías</NavLink>
+        <NavLink to="/admin/users">Usuarios</NavLink>
       </aside>
       <section>
         <Route exact path="/admin">
@@ -21,6 +23,9 @@ const Admin = () => {
         </Route>
         <Route path="/admin/categories">
           <Categories />
+        </Route>
+        <Route path="/admin/users">
+          <Users />
         </Route>
       </section>
     </section>
