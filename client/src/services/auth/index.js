a81642a0c80804230cf573de9d42f757c2304f3a
Aredex <pacuestar@gmail.com>
2020-09-16 11:41:14 -0500

    adding service login

---
+++ client/src/services/auth/index.js
@@ -0,0 +1,1 @@
+export { default as signin } from "./signin";
--- client/src/store/Actions/Users/UsersActions.js
+++ client/src/store/Actions/Users/UsersActions.js
@@ -1,30 +1,27 @@
 import {
-  create,
-  remove,
-  getById,
-  getAll,
-  update,
-  getReviews,
-  ordersByUser,
-  toAdmin,
-  toGuest
+    create,
+    // remove,
+    // getById,
+    // getAll,
+    // update,
+    // getReviews,
+    // ordersByUser,
+    // toAdmin,
+    // toGuest
 } from "services/user";
+import { signin } from "services/auth";
 
 export function createUser(name, email, password, role) {
-  return function (dispatch) {
-    return create(name, email, password, role)
-      .then(function (data) {
-        dispatch({ type: "CREATE_USER", payload: data });
-      });
-  }
+    return function (dispatch) {
+        return create(name, email, password, role).then(function (data) {
+            dispatch({ type: "CREATE_USER", payload: data });
+        });
+    };
 }
 export function getUser(email, password) {
-  return function (dispatch) {
-    return getAll()
-      .then(function (data) {
-        var aux = data.filter((u) => u.email == email && u.password == password)
-        dispatch({ type: "SIGN_IN_USER", payload: aux[0] });
-      });
-  }
+    return function (dispatch) {
+        return signin(email, password).then(function (data) {
+            dispatch({ type: "SIGN_IN_USER", payload: data });
+        });
+    };
 }
-
