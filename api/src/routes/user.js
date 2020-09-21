ccd0f1bab96c5192ba5b2d0892149857575786ad
TheJB4 <juanmer382@gmail.com>
2020-09-21 13:40:25 -0300

    passwordReset

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,84 +1,94 @@
 const router = require("express").Router();
 const {
-    createOne,
-    getAll,
-    getOne,
-    editOne,
-    deleteOne,
+  createOne,
+  getAll,
+  getOne,
+  editOne,
+  deleteOne,
 } = require("../controllers/users");
 const { getOrderByUser } = require("../controllers/users_order");
 const { getAll: getReviews } = require("../controllers/reviews");
 
 router
-    .route("/")
-    .post((req, res) => {
-        const { name, email, password, role } = req.body;
+  .route("/")
+  .post((req, res) => {
+    const { name, email, password, role } = req.body;
 
-        createOne(name, email, password, role)
-            .then((user) => res.json(user).status(201))
-            .catch((err) => res.status(400).json(err));
-    })
-    .get((req, res) => {
-        getAll()
-            .then((users) => res.json(users).status(200))
-            .catch((err) => res.status(404).json(err));
-    });
+    createOne(name, email, password, role)
+      .then((user) => res.json(user).status(201))
+      .catch((err) => res.status(400).json(err));
+  })
+  .get((req, res) => {
+    getAll()
+      .then((users) => res.json(users).status(200))
+      .catch((err) => res.status(404).json(err));
+  });
 
 router
-    .route("/:id")
-    .get((req, res) => {
-        const { id } = req.params;
-
-        getOne(id)
-            .then((user) => res.json(user))
-            .catch((err) => res.status(404).json(err));
-    })
-    .put((req, res) => {
-        const { id } = req.params;
-        const { name, email, password, role } = req.body;
-
-        editOne({ id, name, email, password, role })
-            .then((user) => res.json(user))
-            .catch((err) => res.status(400).json({ err }));
-    })
-    .delete((req, res) => {
-        const { id } = req.params;
-        deleteOne(id)
-            .then((user) => res.json(user).status(200))
-            .catch((err) => res.status(400).json(err));
-    });
-
-router.route("/:id/orders").get((req, res) => {
+  .route("/:id")
+  .get((req, res) => {
     const { id } = req.params;
 
-    getOrderByUser(id)
-        .then((orders) => res.json(orders).status(200))
-        .catch((err) => res.json(err));
+    getOne(id)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(404).json(err));
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+    const { name, email, password, role } = req.body;
+
+    editOne({ id, name, email, password, role })
+      .then((user) => res.json(user))
+      .catch((err) => res.status(400).json({ err }));
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    deleteOne(id)
+      .then((user) => res.json(user).status(200))
+      .catch((err) => res.status(400).json(err));
+  });
+
+router.route("/:id/orders").get((req, res) => {
+  const { id } = req.params;
+
+  getOrderByUser(id)
+    .then((orders) => res.json(orders).status(200))
+    .catch((err) => res.json(err));
 });
 
 // Retorna todas las reviews hechas por el usuario según su id
 router.route("/:id/reviews").get((req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    getReviews({ idUser: id })
-        .then((reviews) => res.json(reviews).status(200))
-        .catch((err) => res.json(err));
+  getReviews({ idUser: id })
+    .then((reviews) => res.json(reviews).status(200))
+    .catch((err) => res.json(err));
 });
 
 router.route("/:id/toadmin").put((req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    editOne({ id, role: "ADMIN" })
-        .then((user) => res.json(user))
-        .catch((err) => res.status(400).json({ err }));
+  editOne({ id, role: "ADMIN" })
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json({ err }));
 });
 
 router.route("/:id/toguest").put((req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    editOne({ id, role: "GUEST" })
-        .then((user) => res.json(user))
-        .catch((err) => res.status(400).json({ err }));
+  editOne({ id, role: "GUEST" })
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json({ err }));
+});
+
+router.route("/:id/resetpassword").post((req, res) => {
+  const { id } = req.params;
+
+  const { newPassword } = req.body;
+
+  editOne({ id, password: newPassword })
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json({ err }));
 });
 
 module.exports = router;
