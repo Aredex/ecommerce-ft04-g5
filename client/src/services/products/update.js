ff2857bbbe5e6d9cbbf083c7317adb4011520363
Sol Battaglia <solbattaglia@gmail.com>
2020-09-22 12:24:27 -0300

    avances

--- client/src/services/products/update.js
+++ client/src/services/products/update.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-export default async function update(id, name, description, price, stock) {
+export default async function update(id, name, description, price, stock, imageUrl) {
   return axios
     .put(`http://localhost:3001/products/${id}`, {
       name,
       description,
       price,
       stock,
+      imageUrl
     })
     .then((response) => {
       return response.data;
