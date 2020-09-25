a4bdfce03936c5b4c2cfbe5f6e708dab32470330
Diego Rodriguez <dierodz91@gmail.com>
2020-09-25 10:08:30 -0300

    create orders profile

--- client/src/pages/Profile/Orders.js
+++ client/src/pages/Profile/Orders.js
@@ -1,10 +1,60 @@
+import Card from 'components/Card';
 import React from 'react'
 import style from "./index.module.scss";
 
 function Orders() {
   return (
     <div>
-
+      <Card>
+        <Card.Header>
+          ultimas 5 ordenes
+        </Card.Header>
+        <Card.Body>
+          <table>
+            <tbody>
+              <tr>
+                <td>5</td>
+                <td>25/10/2020</td>
+                <td>entregada</td>
+                <td>
+                  <div className={style.stepBar}>
+                    <div className={style.step}>
+                      <span>pendiente de pago</span>
+                      <div className={style.dot}>
+                        <i className="fas fa-check" />
+                      </div>
+                    </div>
+                    <div className={style.step}>
+                      <span>confirmada</span>
+                      <div className={style.dot}>
+                        <i className="fas fa-check" />
+                      </div>
+                    </div>
+                    <div className={style.step}>
+                      <span>en preparación</span>
+                      <div className={style.dot}>
+                        <i className="fas fa-check" />
+                      </div>
+                    </div>
+                    <div className={style.step}>
+                      <span>enviada</span>
+                      <div className={style.dot}>
+                        <i className="fas fa-check" />
+                      </div>
+                    </div>
+                    <div className={style.step}>
+                      <span>entregada</span>
+                      <div className={style.dot}>
+                        <i className="fas fa-check" />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
