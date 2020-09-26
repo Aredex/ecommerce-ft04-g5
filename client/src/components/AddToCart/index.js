db20350ad17d1caa22dff09c3409d732c7b806d2
Aredex <pacuestar@gmail.com>
2020-09-26 09:19:58 -0500

    initiating

--- client/src/components/AddToCart/index.js
+++ client/src/components/AddToCart/index.js
@@ -1,51 +1,60 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import style from "./index.module.scss";
 
 const AddToCart = ({
-  onAdd,
-  onSubstract,
-  onSubmit,
-  value,
-  disableAdd,
-  disableSubstract,
+    onAdd,
+    onSubstract,
+    onSubmit,
+    value,
+    disableAdd,
+    disableSubstract,
 }) => {
-  const [added, setAdded] = useState(false);
-  return (
-    <div className={style.inputNumber}>
-      {!added && (
-        <>
-          <input value={value} readOnly></input>
-          <section>
-            <button onClick={onAdd} disabled={disableAdd}>
-              <i className={["fas", "fa-angle-up"].join(" ")}></i>
+    const [added, setAdded] = useState(false);  
+
+    return (
+        <div className={style.inputNumber}>
+            {!added && (
+                <>
+                    <input value={value} readOnly></input>
+                    <section>
+                        <button onClick={onAdd} disabled={disableAdd}>
+                            <i className={["fas", "fa-angle-up"].join(" ")}></i>
+                        </button>
+                        <button
+                            onClick={onSubstract}
+                            disabled={disableSubstract}
+                        >
+                            <i
+                                className={["fas", "fa-angle-down"].join(" ")}
+                            ></i>
+                        </button>
+                    </section>
+                </>
+            )}
+            <button
+                type="submit"
+                className={style.submit}
+                onClick={() => {
+                    if (!added) {
+                        onSubmit();
+                        setAdded(true);
+                    }
+                }}
+            >
+                {!added ? (
+                    <>
+                        <i
+                            className={["fas", "fa-shopping-cart"].join(" ")}
+                        ></i>
+                        {"Añadir al Carro"}
+                    </>
+                ) : (
+                    <>Gracias!</>
+                )}
             </button>
-            <button onClick={onSubstract} disabled={disableSubstract}>
-              <i className={["fas", "fa-angle-down"].join(" ")}></i>
-            </button>
-          </section>
-        </>
-      )}
-      <button
-        type="submit"
-        className={style.submit}
-        onClick={() => {
-          if (!added) {
-            onSubmit();
-            setAdded(true);
-          }
-        }}
-      >
-        {!added ? (
-          <>
-            <i className={["fas", "fa-shopping-cart"].join(" ")}></i>
-            {"Añadir al Carro"}
-          </>
-        ) : (
-          <>Gracias!</>
-        )}
-      </button>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default AddToCart;
--- client/src/pages/Product/index.js
+++ client/src/pages/Product/index.js
@@ -9,103 +9,120 @@ import useOrders from "hooks/useOrders";
 import { useHistory, useParams } from "react-router";
 
 const Product = () => {
-  const [count, setCount] = useState(1);
-  const dispatch = useDispatch();
-  const { id } = useParams();
-  useEffect(() => {
-    dispatch(getProductDetail(id));
-    return () => {
-      dispatch(getProductDetail());
+    const [count, setCount] = useState(1);
+
+    const dispatch = useDispatch();
+
+    const { id } = useParams();
+
+    const products = useSelector(
+        (state) => state.orders?.shoppingCart?.products
+    );
+
+    let TheProductOnCart = null;
+
+    useEffect(() => {
+        // TheProductOnCart = products.find((product) => product.id === id);
+        // if (TheProductOnCart) {
+        //     setCount(TheProductOnCart.amount);
+        // }
+        console.log(products)
+    }, [products]);
+
+    useEffect(() => {
+        dispatch(getProductDetail(id));
+        return () => {
+            dispatch(getProductDetail());
+        };
+    }, [dispatch, id]);
+    const history = useHistory();
+    const product = useSelector((x) => x.ProductsReducer.productDetail);
+    const handleOnAdd = () => {
+        setCount(count + 1);
     };
-  }, [dispatch, id]);
-  const history = useHistory();
-  const product = useSelector((x) => x.ProductsReducer.productDetail);
-  const handleOnAdd = () => {
-    setCount(count + 1);
-  };
 
-  const hableOnSubstract = () => {
-    if (count <= 1) {
-      return 1;
-    }
+    const hableOnSubstract = () => {
+        if (count <= 1) {
+            return 1;
+        }
 
-    setCount(count - 1);
-  };
+        setCount(count - 1);
+    };
 
-  const { addProduct } = useOrders();
+    const { addProduct } = useOrders();
 
-  if (product) {
-    const imageURL = product.images[0]?.url || noImage;
-    return (
-      <div className={style.page}>
-        <div className={style.carusel}>
-          <img width="200" height="200" src={imageURL} alt="" />
-        </div>
-        <div className={style.info}>
-          <div className={style.name}>
-            <h1>{product.name}</h1>
-          </div>
-          <div className={style.price}>
-            <label>$</label>
-            <p>{product.price}</p>
-          </div>
+    if (product) {
+        const imageURL = product.images[0]?.url || noImage;
+        return (
+            <div className={style.page}>
+                <div className={style.carusel}>
+                    <img width="200" height="200" src={imageURL} alt="" />
+                </div>
+                <div className={style.info}>
+                    <div className={style.name}>
+                        <h1>{product.name}</h1>
+                    </div>
+                    <div className={style.price}>
+                        <label>$</label>
+                        <p>{product.price}</p>
+                    </div>
 
-          <ReviewButton reviews={product.reviews} idProduct={product.id} />
-          {product.stock > 0 ? (
-            <AddToCart
-              onAdd={handleOnAdd}
-              onSubstract={hableOnSubstract}
-              value={count}
-              disableAdd={count === product.stock}
-              disableSubstract={count === 1}
-              onSubmit={() =>
-                addProduct(
-                  product.id,
-                  product.name,
-                  product.price,
-                  count,
-                  product.stock
-                )
-              }
-            />
-          ) : (
-              <h1>No contamos con stock</h1>
-            )}
-          <div className={style.category}>
-            <div className={style.separator}>Categorias</div>
-            <section>
-              {product.categories.map((category, key) => (
-                <span
-                  key={key}
-                  onClick={() =>
-                    history.push(`/products?category=${category.id}`)
-                  }
-                >
-                  {category.name}
-                </span>
-              ))}
-            </section>
-          </div>
-          <div className={style.description}>
-            <div className={style.separator}>
-              <span>Descripción</span>
+                    <ReviewButton
+                        reviews={product.reviews}
+                        idProduct={product.id}
+                    />
+                    {product.stock > 0 ? (
+                        <AddToCart
+                            onAdd={handleOnAdd}
+                            onSubstract={hableOnSubstract}
+                            value={count}
+                            disableAdd={count === product.stock}
+                            disableSubstract={count === 1}
+                            onSubmit={() =>
+                                addProduct(
+                                    product.id,
+                                    product.name,
+                                    product.price,
+                                    count,
+                                    product.stock
+                                )
+                            }
+                        />
+                    ) : (
+                        <h1>No contamos con stock</h1>
+                    )}
+                    <div className={style.category}>
+                        <div className={style.separator}>Categorias</div>
+                        <section>
+                            {product.categories.map((category, key) => (
+                                <span
+                                    key={key}
+                                    onClick={() =>
+                                        history.push(
+                                            `/products?category=${category.id}`
+                                        )
+                                    }
+                                >
+                                    {category.name}
+                                </span>
+                            ))}
+                        </section>
+                    </div>
+                    <div className={style.description}>
+                        <div className={style.separator}>
+                            <span>Descripción</span>
+                        </div>
+                        {product.description}
+                    </div>
+                </div>
             </div>
-            {product.description}
-          </div>
-          <div className={style.description}>
-            <div className={style.separator}>
-              <span>Reviews</span>
+        );
+    } else
+        return (
+            <div>
+                <span>Cargando...</span>
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  } else
-    return (
-      <div>
-        <span>Cargando...</span>
-      </div>
-    );
+        );
 };
 
 export default Product;
