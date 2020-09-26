93391aec7a897d66df79556be2c1d6ad6f9db551
Diego Rodriguez <dierodz91@gmail.com>
2020-09-26 12:28:45 -0300

    style 404

--- client/src/pages/PageNotFound/index.js
+++ client/src/pages/PageNotFound/index.js
@@ -4,8 +4,10 @@ import notFound from 'assets/notFound.svg';
 
 function PageNotFound() {
   return (
-    <div>
+    <div className={style.page}>
       <img src={notFound} alt="" />
+      <h2>Ups!</h2>
+      <h3>Página no encontrada</h3>
     </div>
   )
 }
