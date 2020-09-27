d8ba83bc988a4c039d2be3a27c30a6754a1af227
Diego Rodriguez <dierodz91@gmail.com>
2020-09-26 21:19:35 -0300

    add filters in profile/orders

--- client/src/pages/Profile/Orders.js
+++ client/src/pages/Profile/Orders.js
@@ -1,25 +1,20 @@
 import Axios from 'axios';
 import Card from 'components/Card';
 import StepBar from 'components/StepBar';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import style from "./index.module.scss";
 
-function Orders() {
-  const stepData = [
-    { label: 'pendiente de pago', value: 'PENDING_PAYMENT' },
-    { label: 'confirmada', value: 'CONFIRMED' },
-    { label: 'en preparación', value: 'PREPARING' },
-    { label: 'enviada', value: 'SENT' },
-    { label: 'recibida', value: 'DELIVERED' }
-  ]
+const stepData = [
+  { label: 'pendiente de pago', value: 'PENDING_PAYMENT' },
+  { label: 'confirmada', value: 'CONFIRMED' },
+  { label: 'en preparación', value: 'PREPARING' },
+  { label: 'enviada', value: 'SENT' },
+  { label: 'recibida', value: 'DELIVERED' }
+]
 
-  const [orders, setOrders] = useState()
-  useEffect(() => {
-    (async () => {
-      const { data } = await Axios.get(`${process.env.REACT_APP_API}/users/me/orders`)
-      setOrders(data)
-    })()
-  }, [])
+
+function Order({ order, showStatus }) {
+
   const getTotal = (order) => {
     return order && order.products && Array.isArray(order.products)
       ? order.products.reduce((result, { order_product }) => {
@@ -27,35 +22,152 @@ function Orders() {
       }, 0)
       : 0;
   };
-  console.log(orders)
+
+  return <div key={order.id} className={style.order}>
+    <div className={style.info}>
+      <div>
+        <label>Id:</label>
+        <span>{order.id}</span>
+      </div>
+      <div>
+        <label>Fecha:</label>
+        <span>{new Date(order.createdAt).toLocaleDateString()}</span>
+      </div>
+      <div>
+        <label>Importe:</label>
+        <span>{`$ ${parseFloat(getTotal(order)).toFixed(2)}`}</span>
+      </div>
+    </div>
+    {showStatus && <StepBar value={order.status} steps={stepData} />}
+  </div>
+}
+
+function Orders() {
+
+  const [orders, setOrders] = useState()
+
+  useEffect(() => {
+    (async () => {
+      let { data } = await Axios.get(`${process.env.REACT_APP_API}/users/me/orders`)
+      console.log(data)
+      if (Array.isArray(data))
+        data = data.sort((a, b) => {
+          if (a.createdAt > b.createdAt) return -1
+          if (a.createdAt < b.createdAt) return 1
+          return 0
+        })
+      setOrders(data)
+    })()
+  }, [])
+
+  const lastOrders = useMemo(() => {
+    return orders ? orders.slice(0, 5) : undefined
+  }, [orders])
+
+  const pendingPayment = useMemo(() => {
+    if (!orders) return undefined
+    const result = orders.filter(order => order.status === 'PENDING_PAYMENT')
+    return result.length > 0 ? result : undefined
+  }, [orders])
+
+  const confirmed = useMemo(() => {
+    if (!orders) return undefined
+    const result = orders.filter(order => order.status === 'CONFIRMED')
+    return result.length > 0 ? result : undefined
+  }, [orders])
+
+  const preparing = useMemo(() => {
+    if (!orders) return undefined
+    const result = orders.filter(order => order.status === 'PREPARING')
+    return result.length > 0 ? result : undefined
+  }, [orders])
+
+  const sent = useMemo(() => {
+    if (!orders) return undefined
+    const result = orders.filter(order => order.status === 'SENT')
+    return result.length > 0 ? result : undefined
+  }, [orders])
+
+  const delivered = useMemo(() => {
+    if (!orders) return undefined
+    const result = orders.filter(order => order.status === 'DELIVERED')
+    return result.length > 0 ? result : undefined
+  }, [orders])
+
+  console.log(sent)
+
   return (
     <div className={style.orderPage}>
-      <Card>
+      <Card className={style.ordersCard}>
         <Card.Header>
           ultimas 5 ordenes
         </Card.Header>
         <Card.Body>
-          {orders && orders.map(order =>
-            <div key={order.id} className={style.order}>
-              <div className={style.info}>
-                <div>
-                  <label>Id:</label>
-                  <span>{order.id}</span>
-                </div>
-                <div>
-                  <label>Fecha:</label>
-                  <span>{new Date(order.createdAt).toLocaleDateString()}</span>
-                </div>
-                <div>
-                  <label>Importe:</label>
-                  <span>{`$ ${parseFloat(getTotal(order)).toFixed(2)}`}</span>
-                </div>
-              </div>
-              <StepBar value={order.status} steps={stepData} />
-            </div>
+          {lastOrders && lastOrders.map(order =>
+            <Order key={order.id} order={order} showStatus />
           )}
         </Card.Body>
       </Card>
+      {pendingPayment &&
+        <Card className={style.ordersCard}>
+          <Card.Header>
+            ordenes pendientes de pago
+          </Card.Header>
+          <Card.Body>
+            {pendingPayment.map(order =>
+              <Order key={order.id} order={order} />
+            )}
+          </Card.Body>
+        </Card>
+      }
+      {confirmed &&
+        <Card className={style.ordersCard}>
+          <Card.Header>
+            ordenes confirmadas
+        </Card.Header>
+          <Card.Body>
+            {confirmed.map(order =>
+              <Order key={order.id} order={order} />
+            )}
+          </Card.Body>
+        </Card>
+      }
+      {preparing &&
+        <Card className={style.ordersCard}>
+          <Card.Header>
+            ordenes en preparación
+        </Card.Header>
+          <Card.Body>
+            {preparing.map(order =>
+              <Order key={order.id} order={order} />
+            )}
+          </Card.Body>
+        </Card>
+      }
+      {sent &&
+        <Card className={style.ordersCard}>
+          <Card.Header>
+            ordenes enviadas
+        </Card.Header>
+          <Card.Body>
+            {sent.map(order =>
+              <Order key={order.id} order={order} />
+            )}
+          </Card.Body>
+        </Card>
+      }
+      {delivered &&
+        <Card className={style.ordersCard}>
+          <Card.Header>
+            ordenes recibidas
+        </Card.Header>
+          <Card.Body>
+            {delivered.map(order =>
+              <Order key={order.id} order={order} />
+            )}
+          </Card.Body>
+        </Card>
+      }
     </div>
   )
 }
