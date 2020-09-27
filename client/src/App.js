0f990891142de3de03ff35a7253b3a43f53421fb
Diego Rodriguez <dierodz91@gmail.com>
2020-09-27 19:01:15 -0300

    fix GA

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Layout from "components/Layout";
 import Routes from "Routes";
+import { useLocation } from "react-router";
+import ReactGA from 'react-ga';
 
 function App() {
+  const location = useLocation()
+  useEffect(() => { ReactGA.initialize('UA-154949513-1') }, [])
+  useEffect(() => { ReactGA.pageview(location) }, [location])
   return (
     <div className="App">
       <Layout>
