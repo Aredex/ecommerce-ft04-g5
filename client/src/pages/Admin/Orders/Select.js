896d3242191c1041bd02cc0f98306f988d1d6d4b
Sol Battaglia <solbattaglia@gmail.com>
2020-09-28 07:26:10 -0300

    No puedo cambiar el hover del option

--- client/src/pages/Admin/Orders/Select.js
+++ client/src/pages/Admin/Orders/Select.js
@@ -1,29 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { getAllOrders, setDeliveredOrder } from "services/orders";
-import './Style.scss'
-
+import "./Style.scss";
 
 const valores = [
-                "IN CREATION",
-                "CONFIRMED",
-                "REJECTED",
-                "PREPARING",
-                "SENT",
-                "DELIVERED",
-                "FINALIZED"
-                ]
+  "IN CREATION",
+  "CONFIRMED",
+  "REJECTED",
+  "PREPARING",
+  "SENT",
+  "DELIVERED",
+  "FINALIZED",
+];
 
-export default function Select(props) { 
-
+export default function Select(props) {
   return (
-    <div className='container'>
-      <select onChange={(e)=>{props.handleChange(e, props.id, props.address, props.i)}}>
-      {valores.map((val, index)=>{
-        if(props.status === val){
-            return <option value={`${val}`} key={index} disabled selected>{val}</option>
-        }else{
-          return <option value={`${val}`} key={index}>{val}</option>      }            
-      })}
+    <div className="container">
+      <select
+        onChange={(e) => {
+          props.handleChange(e, props.id, props.address, props.i);
+        }}
+      >
+        {valores.map((val, index) => {
+          if (props.status === val) {
+            return (
+              <option
+                class="opciones"
+                value={`${val}`}
+                key={index}
+                disabled
+                selected
+              >
+                {val}
+              </option>
+            );
+          } else {
+            return (
+              <option value={`${val}`} key={index}>
+                {val}
+              </option>
+            );
+          }
+        })}
       </select>
     </div>
   );
